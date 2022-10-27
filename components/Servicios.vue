3b1ace8ef991cf197be4fcd0c96cf2fd9f4a4ab8
HugoBzn <hugobazan1499@gmail.com>
2022-10-27 10:01:58 -0500

    Update button

--- components/Servicios.vue
+++ components/Servicios.vue
@@ -47,17 +47,17 @@
           />
         </div>
       </div>
-      <button
+      <!-- <button
         type="submit"
         class="mt-16 col-start-3 col-end-5 mb-4 transition duration-300 ease-in-out centertext-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
       >
         Ver listado
-      </button>
+      </button> -->
       <button
         class="mt-16 col-start-3 col-end-5 mb-4 transition duration-300 ease-in-out centertext-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
         @click="urlAPI"
       >
-        Armar
+        Ver listado
       </button>
     </form>
   </div>
